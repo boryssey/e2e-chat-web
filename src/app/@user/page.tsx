0a8483552cc699a423fe96c1339708bca8a7ecc4
Borys Melnyk <bor920@gmail.com>
2024-04-25 17:01:49 +0100

    feat: add socket io

--- src/app/@user/page.tsx
+++ src/app/@user/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { socket } from "../socket";
 
 const UserPage = () => {
   console.log("user called?");
   const router = useRouter();
+  const [isConnected, setIsConnected] = useState(false);
+  const [transport, setTransport] = useState("N/A");
 
   const logoutHandler = useCallback(async () => {
     const res = await fetch("http://localhost:3000/auth/logout", {
@@ -22,10 +25,41 @@ const UserPage = () => {
     console.log("logout success");
   }, [router]);
 
+  useEffect(() => {
+    if (socket.connected) {
+      onConnect();
+    }
+
+    function onConnect() {
+      setIsConnected(true);
+      setTransport(socket.io.engine.transport.name);
+
+      socket.io.engine.on("upgrade", (transport) => {
+        setTransport(transport.name);
+      });
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+      setTransport("N/A");
+    }
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+    socket.on("hello", (data: any) => console.log(data, "hello data"));
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+    };
+  }, []);
+
   return (
     <>
       <h1>UserPage</h1>
       <button onClick={() => logoutHandler()}>Logout</button>
+      <p>Status: {isConnected ? "connected" : "disconnected"}</p>
+      <p>Transport: {transport}</p>
     </>
   );
 };
