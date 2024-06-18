5d0067af9f8093fb07debcd92356385d6231295c
Borys Melnyk <bor920@gmail.com>
2024-06-18 17:05:11 +0100

    feat: add color scheme

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    typescript: {
-        ignoreBuildErrors: true,
-    },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.resolve.fallback = { fs: false }
+    }
 
-};
+    return config
+  },
+}
 
-export default nextConfig;
+export default nextConfig
