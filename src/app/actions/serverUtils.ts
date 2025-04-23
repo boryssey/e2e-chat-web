0058358fc373b6cc3d23dca9a7479ba792c7b23c
Borys Melnyk <bor920@gmail.com>
2025-04-23 20:43:43 +0100

    fix: setting cookie in server action

--- src/app/actions/serverUtils.ts
+++ src/app/actions/serverUtils.ts
@@ -5,12 +5,9 @@ import { cookies } from 'next/headers'
 export async function handleServerCookies(headerCookies: string[]) {
   if (headerCookies.length) {
     const cookiePromises = headerCookies.map(async (cookie) => {
-      console.log('cookie', cookie)
-      const [cookieName, ...cookieAttributes] = cookie.split('=')
+      const [cookieName, cookieValue] = cookie.split(';')[0].split('=')
       const cookieStore = await cookies()
-      console.log('cookieName', cookieName)
-      console.log('cookieAttributes', cookieAttributes)
-      cookieStore.set(cookieName, cookieAttributes.join('='), {
+      cookieStore.set(cookieName, cookieValue, {
         httpOnly: true,
       })
     })
